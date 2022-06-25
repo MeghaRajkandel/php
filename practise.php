9302fcc6cc37dbcdb003dae5c1f7d86231afbadd
Megha Raj kandel <75773490+MeghaRajkandel@users.noreply.github.com>
2022-06-25 08:16:06 -0700

    Add files via upload

---
+++ practise.php
@@ -0,0 +1,5 @@
+<?php 
+$numbers = array(“firstnumber” => 1, “secondnumber” => 2,  “thirdnumber” => 3, “fourthnumber” => 4, “fifthnumber” => 5);
+echo $numbers[‘firstnumber’]; //it prints 1
+echo $numbers[‘fifthnumber’]; //it prints 5
+?>
