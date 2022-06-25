828e8f0ee06e3b93bc7404f6a6f5a507d53651a9
Megha Raj kandel <75773490+MeghaRajkandel@users.noreply.github.com>
2022-06-25 08:26:30 -0700

    Create Multidimensionalarray.php

---
+++ Multidimensionalarray.php
@@ -0,0 +1,10 @@
+<?php 
+$marks = array(
+	“mrk” => array(“math”=>97, “science”=>88, “computer”=>92),
+	“sujan” => array(“math”=>70, “science”=>65, “computer”=>99),
+	“sujana” => array(“math”=>46, “science”=>76, “computer”=>75)
+);
+echo $marks[‘mrk’][‘math’];
+echo $marks[‘sujan’][‘math’];
+echo $marks[‘sujana’[‘computer’];
+?>
